91d6b0ef7e4d772ad703312e0c6216628ba412e7
Andrey Glumov <gluman@yandex.ru>
2025-02-09 23:19:32 +0300

    some commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-//
+
+select_data = [
+{label: 'Драма', value: 'drama'},
+{label: 'Комедия', value: 'comedy'},
+{label: 'Фантастика', value: 'sci-fi'}]
+
+
+const film_name = document.getElementById('name')
+const  film_genre = document.getElementById('genre')
+
+console.log(film_name)
+console.log(film_genre)
+select_data.forEach(element => {
+    option = document.createElement('option')
+    console.log(element('label'))
+    console.log(element('value'))
+    option.text = element('label')
+    option.value = element('value')
+    film_genre.add()
+});
